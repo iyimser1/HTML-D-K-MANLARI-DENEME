554f564292079bc5929c0e60a57ddcba6eeb8b7b
iyimser1 <iyimser1@yahoo.com>
2015-10-16 18:36:05 +0300

    css denemeleri
    css örnekleri

---
+++ deneme_css.php
@@ -0,0 +1,29 @@
+<head>
+    <meta charset="utf-8"/>
+    <link type="text/css"rel="stylesheet" href="ugur_css.css">
+    <style>
+        h1 {
+            background-color: darkcyan;
+        }
+        #deneme{
+            background-color: darkslategray;
+        }
+        .caca {
+            background-color: violet;
+        }
+    </style>
+</head>
+
+<span class="caca">caca</span>
+<h1>uğur</h1>
+selam
+<span style="background-color: green;border-width:thick;border-color: cyan;border-style: double">selam</span>
+<span id="deneme">selam</span>
+<span style="background-color: red">selam</span>
+<div style="background-color: yellow">
+    selam
+</div>
+<p>selamlar uğur p tagı</p>
+<textarea name="ugur_id" id="ugur_id" cols="30" rows="10">ugur id</textarea>
+<div class="ugur_clas">ugur clas</div>
+<?php
